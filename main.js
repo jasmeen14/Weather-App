77aa736c65fed10b20852494b5d0c53a05711015
jasmeen14 <jasmeen6199@gmail.com>
2022-04-28 12:41:08 +0200

    changes in html

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',()=>{
+  let long;
+  let lat;
+  if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position =>{
+          //console.log(position);
+          long= position.coords.longitude;
+          lat= position.coords.latitude;
+      });
+  }
+  
+});
