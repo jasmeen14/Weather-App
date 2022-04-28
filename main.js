a7084a2c72bdb13cf5b93d13aecc389ac129475b
jasmeen14 <jasmeen6199@gmail.com>
2022-04-28 17:36:33 +0200

    fetched json file

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-var button = document.querySelector('button')
+var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=0289a213b72fa409ce8ebc9f485c9b86')
-  .then(response=>response.json())
-  .then(data => console.log(data))
+button.addEventListener('click',()=>{
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q='+inputValue.value+'&appid=6d615fbdb0fa1842449678692e31cdd1')
+    .then(response => response.json())
+    .then(data => console.log(data))
 
-  .catch(err => alert("Wrong city name."))
+    
+  
+    .catch(err => alert("Wrong city name."))
+    
+})
+
+
+
+
+
+
+
