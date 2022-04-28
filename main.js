9dbfa88466b526f690de8e524b79d7a9029ed71d
jasmeen14 <jasmeen6199@gmail.com>
2022-04-28 13:14:05 +0200

    created javascript code

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-window.addEventListener('load',()=>{
-  let long;
-  let lat;
-  if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(position =>{
-          //console.log(position);
-          long= position.coords.longitude;
-          lat= position.coords.latitude;
-      });
-  }
-  
-});
+var button = document.querySelector('button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=0289a213b72fa409ce8ebc9f485c9b86')
+  .then(response=>response.json())
+  .then(data => console.log(data))
+
+  .catch(err => alert("Wrong city name."))
